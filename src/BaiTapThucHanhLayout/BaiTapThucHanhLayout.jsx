3d29880d4d572465285c6f923ea44bec065ae61d
Nguyen Hai Dang <danghai23122005@gmail.com>
2024-09-25 22:17:07 +0700

    submit

---
+++ src/BaiTapThucHanhLayout/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from './Header'
+import Body from './Body'
+import Item from './Item'
+import Footer from './Footer'
+
+export default function BaiTapThucHanhLayout() {
+  return (
+    <div>
+        <Header/>
+        <Body/>
+        <Item/>
+        <Footer/>
+    </div>
+  )
+}
---
+++ src/BaiTapThucHanhLayout/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from './Banner'
+
+export default function Body() {
+  return (
+    <div className='py-5'>
+        <div className='container px-lg-5'>
+            <Banner/>
+        </div>
+    </div>
+  )
+}
